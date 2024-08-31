b529107ee7df4077e0a9ae4c38052bdd54c7b1a3
shrikarthik007 <holebagilshrikarthik@gmail.com>
2024-08-31 16:43:52 +0530

    chore: Update npm dependencies for cookie-parser and cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import dotenv from "dotenv"
-
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./env",
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running in port ${process.env.PORT}`)
+        })
+        app.on("error", (err) => {
+            console.log("Error", err)
+        })
+    })
+    .catch((err) => {
+        console.log("Connection Failed", err)
+    })
